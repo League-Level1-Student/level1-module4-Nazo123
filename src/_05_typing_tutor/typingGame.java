3a9581314fabd84c63b403aa7a15a5dd1b643c76
League Student <league@iMac-13.local>
2020-02-27 18:29:08 -0800

    idk

---
+++ src/_05_typing_tutor/typingGame.java
@@ -0,0 +1,13 @@
+package _05_typing_tutor;
+
+import javax.swing.JFrame;
+
+public class typingGame {
+static JFrame frame = new JFrame();
+	public static void main(String[] args) {
+		
+		frame.setTitle("eere");
+		frame.setVisible(true);
+	}
+	
+}
